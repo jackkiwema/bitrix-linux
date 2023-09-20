7a2550b7342cf1ab7446cfff5c1198b8318624bd
Jack Kiwema <jackkiwema@gmail.com>
2023-09-20 15:41:12 +0300

    Moved Debian to Stable version

---
+++ debian1/.settings.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'smtp' => [
+        'value' => [
+            'enabled' => true,
+            'debug' => true,
+            'log_file' => '/var/mailer.log' 
+        ]	 
+    ]
+];
+
+
+'smtp' =>
+  array (
+    'value' =>
+    array (
+      'enabled' => true,
+      'debug' => true,
+      'log_file' => '/var/mailer.log',
+    ),
+  ),
---
+++ debian1/readme.md
@@ -0,0 +1,7 @@
+---
+Nginx Configuration
+---
+
+```bash
+# Nginx `/etc/nginx/sites-available/default.conf` similar to `/etc/nginx/bx/site_avaliable/s1.conf`
+```
